161cd39f55fc13a63a94e57e6d37f9756e59a314
Amitav <amitavdebnath2000@gmail.com>
2021-07-12 00:06:57 +0600

    add

---
+++ Add.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+public class Add {
+    public static void main(String[] args) {
+    int a,b,c;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter any two numbers to add : ");
+    a=sc.nextInt();
+    b=sc.nextInt();
+    c=a+b;
+    System.out.println("The sum is "+c);
+
+    }
+    
+}
