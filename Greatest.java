8813a946e5d41c3b06b6086ff2298fb485eb1d2b
Amitav <amitavdebnath2000@gmail.com>
2021-07-13 01:26:39 +0600

    commit on conditional statements

---
+++ Greatest.java
@@ -0,0 +1,18 @@
+public class Greatest{
+    public static void main(String args[])
+    {
+        int a=6,b=6,c=6;
+        if (a>b && a>c)
+        {
+            System.out.println("A is the gratest");
+        }
+        else if (b>c){
+            System.out.println("B is the greatest");
+            
+        }
+        else {
+            System.out.println("C is the greatest");
+        }
+    }
+}
+    
