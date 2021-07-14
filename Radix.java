018ceae43be9c7543e64659fd99a58a138b76a6d
Amitav <amitavdebnath2000@gmail.com>
2021-07-15 00:22:41 +0600

    completed radix

--- Radix.java
+++ Radix.java
@@ -11,9 +11,21 @@ num=sc.nextLine();
 if (num.matches("[01]+"))
 {
     System.out.println("Binary Radix=2");
-
 }
-
-    
+ else if (num.matches("[0-7]+"))
+    {
+    System.out.println("Octal Radix=8");
+}
+else if (num.matches("[0-9]+"))
+    {
+    System.out.println("Decimal Radix=10");
+}
+else if (num.matches("[0-9A-F]+"))
+    {
+    System.out.println("Hexa-Decimal Radix=16");
+}
+else {
+    System.out.println("This is not a number");
+}
 }
 }
