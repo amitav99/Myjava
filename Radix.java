1831e8489ad0ad8a12f03d4e7ef24e1ae563795b
Amitav <amitavdebnath2000@gmail.com>
2021-07-14 23:46:44 +0600

    commit on radix

---
+++ Radix.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Radix {
+    public static void main (String args[])
+
+    {
+    String num;
+Scanner sc = new Scanner(System.in);
+System.out.println("enter a number : ");
+num=sc.nextLine();
+
+if (num.matches("[01]+"))
+{
+    System.out.println("Binary Radix=2");
+
+}
+
+    
+}
+}
