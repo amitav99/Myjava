432d034c0ce48b7667126cd03f7beb8b5116e450
Amitav <amitavdebnath2000@gmail.com>
2021-07-16 15:23:47 +0600

    counting the number of digits

---
+++ Count.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Count
+{
+    public static void main(String args[])
+    {
+        int c=0, n;
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the number ");
+        n=sc.nextInt();
+        while(n>0)
+        {
+           // rem = n%10;
+            n=n/10;
+            c++;
+         }
+         System.out.println(c );
+    
+    }
+}
