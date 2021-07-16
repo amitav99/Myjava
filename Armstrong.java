72788c7a6a53b66b004aee0188d5659e5cd3f229
Amitav <amitavdebnath2000@gmail.com>
2021-07-16 15:43:22 +0600

    commit on Armstrong number

--- Armstrong.java
+++ Armstrong.java
@@ -1,3 +1,24 @@
+import java.util.*;
+
 public class Armstrong {
-    
+    public static void main(String[] args) {
+    int num, n, rem, sum=0 ;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number to check for armstrong : ");
+    num = sc.nextInt();
+    n=num;
+    while (n>0)
+    {
+        rem=n%10;
+        sum=sum+(rem*rem*rem);
+        n=n/10;
+    }
+    if (num==sum)
+    {
+        System.out.println("Armstrong number");
+    }
+    else{
+        System.out.println("Not a armstrong number");
+    }
+}
 }
