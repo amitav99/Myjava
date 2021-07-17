dcad06046c02b27850c9ba773c2fed45ab6ba31e
Amitav <amitavdebnath2000@gmail.com>
2021-07-17 21:44:47 +0600

    left rotation of an array

---
+++ Rotation.java
@@ -0,0 +1,22 @@
+public class Rotation {
+    public static void main (String args[])
+    {
+        int i ;
+        int arr[]={1,2,3,4,5}; 
+        int temp =arr[0];
+
+        for (int x : arr)
+        System.out.print(x +" ");
+        
+        for (i=1; i<arr.length; i++)
+        {
+             arr[i-1]=arr[i];
+             
+        }
+        arr[arr.length-1]=temp;
+        System.out.println(" ");
+        System.out.println("Array after rotation is : ");
+        for (int x : arr)
+        System.out.print(x+" ");
+    }
+}
--- Sec_Largest.java
+++ Sec_Largest.java
@@ -3,7 +3,7 @@ public class Sec_Largest
     public static void main (String args[])
     {
         int i, max1, max2;
-        int arr[] = {9, 5, 13, 5, 7, 45};
+        int arr[] = {9, 5, 13, 5, 45, 15};
         max1= max2 = arr[0];
         for (i=0; i<arr.length; i++)
         {
