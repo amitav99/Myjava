a039029710fb22f1e5fbbca62e059b86c97353e1
Amitav <amitavdebnath2000@gmail.com>
2021-07-21 00:02:11 +0600

    final commit on prime

--- Prime.java
+++ Prime.java
@@ -1,14 +1,27 @@
 public class Prime{
-    static boolean isprime(){
+    static boolean isprime(int n){
+        if (n<=1)
+            return false;
+        for (int i=2; i<=n/2;i++)
+            if (n%i==0)
+            return false;
+        return true;
             
         }
         
-    public static void main(String args[]) {
-        static boolean isprime(){
-
-        }
-        
+    public static void main(String args[]) 
+    {
+    if (isprime(11))
+    {
+        System.out.println("prime");
     }
+    else
+    {
+        System.out.println("not prime");
+    }
+
+
+    }
+        
 }
-    
-}
+
