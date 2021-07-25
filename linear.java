2d4998b40a2ea27899e5f9d7f0d17b02a947d20e
Amitav <amitavdebnath2000@gmail.com>
2021-07-26 00:27:52 +0600

    complete linear search

--- linear.java
+++ linear.java
@@ -1,3 +1,21 @@
-public class linear {
+public class Linear {
+    static public int linear_search(int arr[], int key)
+    {
+        for (int i=0; i<arr.length;i++)
+        {
+            if (arr[i]==key)
+            {
+                return i+1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main (String args[])
+    {
+        int ar[]={3,4,7,8,6};
+        int key = 2;
+        System.out.println(key +" found at index "+ linear_search(ar, key));
+    }
     
 }
