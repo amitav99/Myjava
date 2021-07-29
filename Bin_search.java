ea15d36fc1fb6c4fd828d678c7d37b4861723546
Amitav <amitavdebnath2000@gmail.com>
2021-07-29 15:51:29 +0600

    binary search

--- Bin_search.java
+++ Bin_search.java
@@ -1,3 +1,29 @@
 public class Bin_search {
+    public static void main (String args[])
+    {
+        int arr[]={1,2,4,5,6,8,9};
+        int key=1;
+        int n = arr.length-1;
+        int low = 0, high = n-1;
+        
+        while(low<high)
+        {
+            int mid = (low+high)/2;
+            if (key==arr[mid])
+            {
+                System.out.println("found");
+                break;
+            }
+            else if (key>arr[mid])
+            {
+                low=mid+1;
+            }
+            else 
+            {
+                high=mid-1;
+            }
+          
+        }
+    }
     
 }
