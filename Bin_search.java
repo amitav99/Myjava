1ebf9e21d9fc8d31c762529ed380c2db81b055c0
Amitav <amitavdebnath2000@gmail.com>
2021-07-29 18:03:28 +0600

    final commit

--- Bin_search.java
+++ Bin_search.java
@@ -1,18 +1,14 @@
 public class Bin_search {
-    public static void main (String args[])
+
+
+    public static int bin_search(int arr[], int key, int low, int high)
     {
-        int arr[]={1,2,4,5,6,8,9};
-        int key=1;
-        int n = arr.length-1;
-        int low = 0, high = n-1;
-        
         while(low<high)
         {
             int mid = (low+high)/2;
             if (key==arr[mid])
             {
-                System.out.println("found");
-                break;
+                return mid;
             }
             else if (key>arr[mid])
             {
@@ -22,8 +18,28 @@ public class Bin_search {
             {
                 high=mid-1;
             }
+    
+        }
+        return -1;
+    }
           
+    
+    public static void main (String args[])
+    {
+        int arr[]={1,2,4,5,6,8,9};
+        int key=99;
+        int n = arr.length-1;
+        int low = 0, high = n-1;
+       int bin_search = bin_search(arr,key,low, high);
+        if (bin_search == -1)
+        {
+            System.out.println("The element is not present");
+        }
+        else
+        {
+            System.out.println("The element present");  
         }
     }
     
 }
+
