0bd98fae1750fd0199b5b3decf41bb195378e95f
Amitav <amitavdebnath2000@gmail.com>
2021-07-31 01:05:29 +0600

    printing elements of the linked list

--- Singly_linkedlist.java
+++ Singly_linkedlist.java
@@ -1,26 +1,41 @@
-public class Singly_linkedlist {
-    private  ListNode head;//instance variable
-    private static class ListNode
+public class Singly_linkedlist{
+    Node head; //instance variable 
+    static class Node
     {
         private int data;
-        private ListNode next;
-
-    public ListNode(int data)
+        private Node next;
+    
+    public Node (int data)
     {
-        this.data =data;
-        this.next =null;
+        this.data= data;
+        this.next=null;
     }
     }
+    public void display()
+    {
+        Node current = head;
+        while (current!=null)
+        {
+            System.out.print(current.data +"-->");
+            current=current.next;
+        }
+        System.out.print("null");
+    }
+
+    //main function
     public static void main (String args[])
     {
-    Singly_linkedlist ob = new Singly_linkedlist();
-    ob.head = new ListNode(10);
-    ListNode second = new ListNode(5);
-    ListNode third = new ListNode(8);
-    ListNode fourth = new ListNode(12);
+        //creating object of the class linked list
+    Singly_linkedlist ob =new Singly_linkedlist();
+    ob.head = new Node(1);
+    Node second = new Node(2);
+    Node third = new Node(3);
+    Node fourth = new Node(4);
 
-    ob.head.next = second;
-    second.next = third;
-    third.next = fourth;
-}
+    ob.head.next=second;
+    second.next=third;
+    third.next=fourth;
+
+    ob.display();
+    }
 }
