7f7363e00c337ec81ff10942ebd52080e2db2835
Amitav <amitavdebnath2000@gmail.com>
2021-08-08 23:56:06 +0600

    commit on insertion sort

--- Insertionsort.java
+++ Insertionsort.java
@@ -1,3 +1,32 @@
 public class Insertionsort {
+    public void sort(int arr[])
+    {
+        int n = arr.length;
+        for (int i=1; i<n; i++)
+        {
+            int key = arr[i];
+            int j = arr[i-1];
+            while(i>0 && 
+        }
+
+    }
+    public void printArray(int arr[])
+    {
+        int n = arr.length ;
+        for (int i=0; i<n; i++)
+        {
+            System.out.print (arr[i]+" ");
+        }
+
+
+    }
+    public static void main (String args[])
+    {
+        Insertionsort ob= new Insertionsort();
+        int arr = {9,5,23,8};
+        ob.sort(arr);
+        System.out.println ("The sorted array(Insertion sort) is : ");
+        ob.printArray(arr);
+    }
     
 }
