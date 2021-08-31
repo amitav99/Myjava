0595ffd52ec7579c1c48909f5c8bceb4e22c7892
Amitav <amitavdebnath2000@gmail.com>
2021-08-31 23:34:00 +0600

    new commit

--- Add.java
+++ Add.java
@@ -5,11 +5,11 @@ public class Add {
     public static void main(String[] args) {
     int a,b,c;
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter any two numbers to add : ");
+    System.out.println("Enter any two numbers to add : "); // the additon of two number
     a=sc.nextInt();
     b=sc.nextInt();
     c=a+b;
-    System.out.println("The sum is "+c);
+    System.out.println("The sum is "+c);//printing sum
 
     }
     
