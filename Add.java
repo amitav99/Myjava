08cec562d6c42e0a544ed7ec84baf74a034c3e45
Amitav <amitavdebnath2000@gmail.com>
2021-09-07 22:50:16 +0600

    new commit

--- Add.java
+++ Add.java
@@ -3,18 +3,19 @@ import java.util.Scanner;
 import java.util.Scanner;
 public class Add {
     public static void main(String[] args) {
-    int a,b,c, d, m;
+    int a,b,c, d, m, n;
     Scanner sc = new Scanner(System.in); // for taking user input
     System.out.println("Enter any two numbers to add : "); // the additon of two number
     a=sc.nextInt();
     b=sc.nextInt();
     c=a+b;//adding
     d =a-b;
-    m= a*b;
+    m = a*b;
+    n = a/b;
     System.out.println("The sum is "+c);//printing sum
     System.out.println("The difference is "+ d);
     System.out.println("The product is "+m);
-
+    System.out.println("The result is "+n);
     }
     
 }
